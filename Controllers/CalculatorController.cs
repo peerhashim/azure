d1bf5d0ec7e235b0cab228bdaf34190549a8eba3
peerhashim369 <peerhashim369@gmail.com>
2026-04-14 13:24:01 +0530

    Calculate controller

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Dto;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpPost("add")]
+        public IActionResult Add([FromBody] AddDto request)
+        {
+            if (request == null)
+            {
+                return BadRequest("Invalid request payload.");
+            }
+            double result = request.Number1 + request.Number2;
+            return Ok(new { Result = result });
+        }
+    }
+}
---
+++ Dto/AddDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Dto
+{
+    public class AddDto
+    {
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+    }
+}
