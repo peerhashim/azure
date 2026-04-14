67c8f80c8c734c998d29b55a8fe0d4167136c97c
peerhashim369 <peerhashim369@gmail.com>
2026-04-14 13:17:06 +0530

    add swagger in production

--- Program.cs
+++ Program.cs
@@ -17,11 +17,11 @@ namespace WebApplication1
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+     /*       if (app.Environment.IsDevelopment())
+            {*/
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+         /*   }*/
 
             app.UseHttpsRedirection();
 
